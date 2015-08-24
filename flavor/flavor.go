199b0f3723cda2765545a295654077b55169fe22
Kishorekumar Neelamegam <nkishore@megam.io>
2015-08-24 18:25:20 +0530

    See my comments in this file to restructure code

--- flavor/flavor.go
+++ flavor/flavor.go
@@ -1,3 +1,4 @@
+//Rename this file to template.go
 package flavor
 
 import (
@@ -6,6 +7,9 @@ import (
 	"github.com/megamsys/opennebula-go/xmlUtil"
 )
 
+//Rename this to TemplateReq (an external entity wants the information of a template)
+//Any callee will formulate this structure and ask to get that template.
+//Refer my thought process below, the callee will not sent TemplateId.
 type FlavorOpts struct {
 	TemplateName string
 	TemplateId   int
@@ -16,9 +20,25 @@ type FlavorOpts struct {
  * Given a templateId it would return the XML of that particular template
  *
  */
+/*Rename this method to Get : Gets a single template by template name.
+//There is a problem with creating client everywhere. why should i provide an endpoint everytime.
+What might be a better way to do this externally, lets say i do
 
+// If the rpc client can't be connected then we can stop here. 
+client, err := NewXMLRPCClient(endpoint,userid, password)  
+tr     := template.NewTemplateReq(...., client)
+_, err := tr.Get()
+
+func (t *TemplateReq) Get() ([]interface{}, error) {
+	res, err := t.client.Invoke("one.templatepool.info", t)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+*/
 func (flavor *FlavorOpts) GetTemplate(endpoint string, key string) ([]interface{}, error) {
-
+	//The 
 	client, err := api.RPCClient(endpoint)
 	if err != nil {
 		fmt.Println(err)
