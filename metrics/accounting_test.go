e84a9a23fff024e1cda997be429f514d1e8916fe
vijaykanthm28 <mvijaykanth@megam.io>
2016-11-08 14:28:34 +0530

    accounting request

--- metrics/accounting_test.go
+++ metrics/accounting_test.go
@@ -1,9 +1,9 @@
 package metrics
-/*
+
 import (
+	"encoding/xml"
 	"testing"
 	"time"
-
 	"github.com/megamsys/opennebula-go/api"
 	"gopkg.in/check.v1"
 )
@@ -22,16 +22,19 @@ var _ = check.Suite(&S{})
 
 func (s *S) SetUpSuite(c *check.C) {
 	cm := make(map[string]string)
-	cm[api.ENDPOINT] = "http://localhost:2633/RPC2"
+	cm[api.ENDPOINT] = "http://192.168.0.118:2633/RPC2"
 	cm[api.USERID] = "oneadmin"
-	cm[api.PASSWORD] = "yourWuOtHij3"
+	cm[api.PASSWORD] = "WuedmopFupt6"
 	s.cm = cm
 }
 
 func (s *S) TestGet(c *check.C) {
 	client, _ := api.NewClient(s.cm)
 	vm := Accounting{Api: client, StartTime: time.Now().Add(-10 * time.Minute).Unix(), EndTime: time.Now().Unix()}
-	_, error := vm.Get()
+	res, error := vm.Get()
+	c.Assert(error, check.IsNil)
+  se := []byte(res.(string))
+	ons := &OpenNebulaStatus{}
+	error = xml.Unmarshal(se, ons)
 	c.Assert(error, check.IsNil)
 }
-*/
