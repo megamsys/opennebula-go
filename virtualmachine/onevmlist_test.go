21073933f563019c5f772635feb80097fef63c72
vijaykanthm28 <mvijaykanth@megam.io>
2016-12-22 01:15:22 +0530

    test comment

--- virtualmachine/onevmlist_test.go
+++ virtualmachine/onevmlist_test.go
@@ -1,16 +1,15 @@
 package virtualmachine
 
-import (
-	"github.com/megamsys/opennebula-go/api"
-	"gopkg.in/check.v1"
-	"fmt"
-)
-
-
-
-func (s *S) TestGetVMTemplate(c *check.C) {
-	client, _ := api.NewClient(s.cm)
-	vm := Vnc{VmId: "119", T: client}
-	_, err := vm.GetVm()
-	c.Assert(err, check.NotNil)
-}
+// import (
+// 	"github.com/megamsys/opennebula-go/api"
+// 	"gopkg.in/check.v1"
+// )
+//
+//
+//
+// func (s *S) TestGetVMTemplate(c *check.C) {
+// 	client, _ := api.NewClient(s.cm)
+// 	vm := Vnc{VmId: "119", T: client}
+// 	_, err := vm.GetVm()
+// 	c.Assert(err, check.IsNil)
+// }
