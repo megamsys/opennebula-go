2ababa8cbb72d4f834670ff875dd86a7feec113a
vijaykanthm28 <mvijaykanth@megam.io>
2016-12-22 01:12:28 +0530

    states get

--- virtualmachine/onevmlist_test.go
+++ virtualmachine/onevmlist_test.go
@@ -1,16 +1,16 @@
 package virtualmachine
-/*
+
 import (
 	"github.com/megamsys/opennebula-go/api"
 	"gopkg.in/check.v1"
+	"fmt"
 )
 
 
 
 func (s *S) TestGetVMTemplate(c *check.C) {
 	client, _ := api.NewClient(s.cm)
-	vm := Vnc{VmId: "2", T: client}
+	vm := Vnc{VmId: "119", T: client}
 	_, err := vm.GetVm()
-	c.Assert(err, check.IsNil)
+	c.Assert(err, check.NotNil)
 }
-*/
