037e5a5199f3173be1ad572e15bb7240c41641af
vijaykanthm28 <mvijaykanth@megam.io>
2017-03-24 19:13:11 +0530

    get vm snapshots states and moved to single file

--- snapshot/snapshot_test.go
+++ snapshot/snapshot_test.go
@@ -18,27 +18,24 @@ var _ = check.Suite(&S{})
 
 func (s *S) SetUpSuite(c *check.C) {
 	cm := make(map[string]string)
-	cm[api.ENDPOINT] = "http://192.168.0.116:2633/RPC2"
+	cm[api.ENDPOINT] = "http://192.168.0.100:2633/RPC2"
 	cm[api.USERID] = "oneadmin"
-	cm[api.PASSWORD] = "5suvJafOtper"
+	cm[api.PASSWORD] = "asdf"
 	s.cm = cm
 }
 
-// func (s *S) TestCreateSnapshot(c *check.C) {
-// 	cl, _ := api.NewClient(s.cm)
-// 	v := Snapshot{
-// 	 VMId: 333,
-//   DiskId:         0,
-// 		DiskDiscription: "backy_test",
-//   T:     cl,
-// }
-
-// 	c.Assert(v, check.NotNil)
-// 	res, err := v.CreateSnapshot()
-// 	fmt.Println(res)
-// 	err = nil
-// 	c.Assert(err, check.NotNil)
-// }
+func (s *S) TestCreateSnapshot(c *check.C) {
+	cl, _ := api.NewClient(s.cm)
+	v := Snapshot{
+		VMId:            87,
+		DiskId:          0,
+		DiskDiscription: "backy_test",
+		T:               cl,
+	}
+	c.Assert(v, check.NotNil)
+	_, err := v.CreateSnapshot()
+	c.Assert(err, check.IsNil)
+}
 
 // func (s *S) TestDeleteSnapshot(c *check.C) {
 // 	cl, _ := api.NewClient(s.cm)
--- virtualmachine/virtualmachine_test.go
+++ virtualmachine/virtualmachine_test.go
@@ -1,11 +1,10 @@
 package virtualmachine
 
-/*
 import (
+	"fmt"
 	"github.com/megamsys/opennebula-go/api"
 	"gopkg.in/check.v1"
 )
-
 
 // func (s *S) TestGetByName(c *check.C) {
 // 	client, _ := api.NewClient(s.cm)
@@ -15,12 +14,35 @@ import (
 // 	c.Assert(err, check.NotNil)
 // }
 
-/*
-func (s *S) TestGetByPort(c *check.C) {
+func (s *S) TestGet(c *check.C) {
 	client, _ := api.NewClient(s.cm)
-	vm := Vnc{ T: client, VmId: "743" }
+	vm := Vnc{T: client, VmId: "877"}
 	b, err := vm.GetVm()
-	fmt.Println(b)
-	c.Assert(err, check.NotNil)
+	c.Assert(err, check.IsNil)
 }
-*/
+
+//
+// func (s *S) TestGetVMTemplate(c *check.C) {
+// 	client, _ := api.NewClient(s.cm)
+// 	vm := Vnc{VmId: "177", T: client}
+// 	_, err := vm.GetVm()
+// 	c.Assert(err, check.IsNil)
+// }
+//
+// func (s *S) TestAttachNic(c *check.C) {
+// 	client, _ := api.NewClient(s.cm)
+//   vm := Vnc{VmId: "425", T: client}
+//   err := vm.AttachNic("ipv4-pri")
+//   res, err := vm.GetVm()
+//   fmt.Println("Error :", err, "\n", res.VmTemplate)
+// 	c.Assert(nil, check.NotNil)
+// }
+
+// func (s *S) TestDetachNic(c *check.C) {
+// 	client, _ := api.NewClient(s.cm)
+//   vm := Vnc{VmId: "425", T: client}
+// 	nic := 3
+//   err := vm.DetachNic(nic)
+//   fmt.Println("Error :", err, "\n")
+// 	c.Assert(nil, check.NotNil)
+// }
